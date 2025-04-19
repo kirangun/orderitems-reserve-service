bb48b858424f373262c7284bc6a44f283cb56c70
Kiran Gundeti <kirangundeti@yahoo.com>
2025-04-19 23:40:49 +0530

    changes for processing order

---
+++ src/main/java/com/azprc/itemreserver/pojo/Item.java
@@ -0,0 +1,19 @@
+package com.azprc.itemreserver.pojo;
+
+public class Item {
+    private String productId;
+    private int quantity;
+    private double price;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
